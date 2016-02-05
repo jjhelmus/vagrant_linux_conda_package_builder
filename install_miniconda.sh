09b3847dfd2ad8a0c590202e3ec4f9204f7579cf
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-05 14:59:10 -0600

    New miniconda path

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -5,7 +5,7 @@ set -x
 cp /vagrant/vendor/Miniconda-latest-Linux-x86.sh .
 chmod +x Miniconda-latest-Linux-x86.sh
 ./Miniconda-latest-Linux-x86.sh -b
-export PATH=/home/vagrant/miniconda/bin:$PATH
-echo "export PATH=/home/vagrant/miniconda/bin:\$PATH " >> ~/.bashrc;
+export PATH=/home/vagrant/miniconda2/bin:$PATH
+echo "export PATH=/home/vagrant/miniconda2/bin:\$PATH " >> ~/.bashrc;
 conda update --yes conda
 conda install --yes conda-build jinja2
